03edb3b9f0cf2c61d4ddd9ea2028771286e39c4b
Gavin Wood <gavinneilwood@gmail.com>
2019-02-07 14:22:35 +0000

    new signalling version that differentiates between two buttons held properly.

--- signalling.py
+++ signalling.py
@@ -17,12 +17,16 @@ SignalStrength = -70
 
 global colour
 colour = 0
-global count
-count = 0
 
 receiveColour = 0
 receiveState = 0
-receiveCount = 0
+
+isAUp = 1
+isADown = 0
+isBUp = 1
+isBDown = 0
+isTogether = 0
+wasTogether = 0
 
 def getHeaderBytes():
     return "msg"
@@ -35,9 +39,8 @@ machineID = "".join("%02x" % i for i in machine.unique_id())
 print("Machine ID: " + getMachine() )
 
 def updateState():
-    global count
     global colour
-    message = getHeaderBytes() + getMachine() + ",{:d},{:d}".format(count, colour)
+    message = getHeaderBytes() + getMachine() + ",{:d}".format(colour)
     radio.send(message)
 
 while True:
@@ -56,49 +59,76 @@ while True:
             
             debugRSSI = "{}".format(rssi)
 
-            recvMachineID, b, c = finalmsg.split(',')
+            recvMachineID, b = finalmsg.split(',')
             
             # print("Machine ID: " + a )
-            receiveCount = int(b)
-            receiveColour = int(c)
-           
-            # update our values if it's a broadcast we haven't seen
-            if receiveCount > count:
+            receiveColour = int(b)
+
+            # get the date time
+            now = utime.ticks_ms()
+            nowStr = str(now)
                 
-                # get the date time
-                now = utime.ticks_ms()
-                nowStr = str(now)
+            # construct the serial message
+            serialMsg = recvMachineID + "," + nowStr + "," + debugRSSI + ",{:d}".format(colour)
+            print(serialMsg) # if we want to collect what is being sent
                 
-                # construct the serial message
-                serialMsg = recvMachineID + "," + nowStr + "," + debugRSSI + ",{:d},{:d}".format(count, colour)
-                print(serialMsg) # if we want to collect what is being sent
-                
-                # db -47 is close
-                # db -100 is 12m 
-                if rssi > SignalStrength:
-                    music.play('A')
-                    colour = receiveColour
-                
-                count = receiveCount
+            # db -47 is close
+            # db -100 is 12m 
+            if rssi > SignalStrength:
+                music.play('A')
+                colour = receiveColour
                     
         incoming = radio.receive_full()
 
-    # elapsed_time = running_time() - start_time
-    # if elapsed_time > 100:
-    #     start_time = running_time()
-    #     updateState()
+    isA = button_a.is_pressed()
+    isB = button_b.is_pressed()
         
-    if (button_a.was_pressed() | button_b.was_pressed()):
-        count = count + 1
-        colour = colour + 1
+    # ------------------------------------------
+    # New algorithmn for detecting button presses
+    if not isA and not isB:
+        if isTogether:
+            wasTogether = 1
+            isTogether = 0
+        else:
+            wasTogether = 0
 
-        if colour > 1:
-            colour = 0
-            
-        updateState()
+    isAUp = 0
+    isBUp = 0
     
+    # Are either A or B held?
+    if isA:
+        isAUp = 0
+        isADown = 1
+    if isB:
+        isBUp = 0
+        isBDown = 1
+
+    # Were they released?
+    if ((not isA) and isADown):
+        isAUp = 1
+        isADown = 0
+    if ((not isB) and isBDown):
+        isBUp = 1
+        isBDown = 0
+    if isA and isB:
+        isTogether = 1
+    # ------------------------------------------
+    
+    # were either A or B released?
+    if isAUp and (isTogether==0):
+        colour = 0
+        updateState()
+    if isBUp and (isTogether==0):
+        colour = 1
+        updateState()
+    if wasTogether:
+        colour = 2
+        updateState()
+        
+    # Show a pattern
     if colour == 0:
         display.show(Image.HAPPY)
     if colour == 1:
         display.show(Image.SAD)
-        
+    if colour == 2:
+        display.show(Image.SURPRISED)    
